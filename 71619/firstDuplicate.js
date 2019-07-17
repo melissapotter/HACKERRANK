97b1582a90f8377dcd6b09b68315c434f14c6593
Melissa <melissapotter15@gmail.comgit confid --global user.name Melissagit config --global user.email melissapotter15@gmail.com>
2019-07-16 20:35:33 -0400

    added first duplicate

--- 71619/firstDuplicate.js
+++ 71619/firstDuplicate.js
@@ -2,11 +2,9 @@ function containsDuplicates(a) {
   for (var i = 0; i < a.length; i++) {
     for (var j = i + 1; j < a.length; j++) {
       if (a[i] == a[j]) {
-        return true;
+        return " The first duplicate found was " + a[j];
       }
     }
   }
   return false;
 }
-
-// print the first duplicate that is found
