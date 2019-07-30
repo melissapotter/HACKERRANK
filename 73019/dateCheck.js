c0135cece81aa604a3da4b799505264f7671597f
melissapotter <melissapotter15@gmail.com>
2019-07-30 18:34:05 -0400

    logged date

--- 73019/dateCheck.js
+++ 73019/dateCheck.js
@@ -1,1 +1,8 @@
-function dateCheck(d)
+var d = new Date();
+var month = d.getMonth() + 1;
+var year = d.getFullYear();
+var date = (month + "." + year);
+
+// function dateCheck() {
+console.log(date);
+// }
