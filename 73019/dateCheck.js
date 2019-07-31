3bb79e12d660abe1413e9548cf86b41ee996d5b2
melissapotter <melissapotter15@gmail.com>
2019-07-30 20:29:52 -0400

    added params

--- 73019/dateCheck.js
+++ 73019/dateCheck.js
@@ -3,9 +3,11 @@ const month = d.getMonth() + 1;
 const year = d.getFullYear();
 
 function dateCheck(m, y) {
-  yearCalc = (year - y);
-  monthCalc = (((month - m) / 12));
-  console.log((yearCalc + monthCalc).toFixed(2));
+  if ((1 <= m <= 12) && (1900 <= y <= year)) {
+    yearCalc = (year - y);
+    monthCalc = (((month - m) / 12));
+    console.log((yearCalc + monthCalc).toFixed(2));
+  }
 }
 
-dateCheck(7, 2018); 
+dateCheck(7, 2000); 
